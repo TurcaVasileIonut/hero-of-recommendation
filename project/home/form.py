5453d13b2fb1fdc7b4dde0c24b1e3cc7945f2b3f
TurcaVasileIonut <69684161+TurcaVasileIonut@users.noreply.github.com>
2021-11-15 18:59:28 +0200

    Delete project/home directory

--- project/home/form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField
-
-
-class MessageForm(Form):
-    """
-    Creates the textbox for search
-    """
-    search = TextField('Search a book to review')
